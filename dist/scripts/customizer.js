771ba7ff0d21b76e3b38cad8e817d0ff1f7fe224
edward01s2 <edward.01s2@gmail.com>
2020-04-05 23:12:26 -0500

    Initial

---
+++ dist/scripts/customizer.js
@@ -0,0 +1,41 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["/scripts/customizer"],{
+
+/***/ "./resources/assets/scripts/customizer.js":
+/*!************************************************!*\
+  !*** ./resources/assets/scripts/customizer.js ***!
+  \************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+/**
+ * This file allows you to add functionality to the Theme Customizer
+ * live preview. jQuery is readily available.
+ *
+ * {@link https://codex.wordpress.org/Theme_Customization_API}
+ */
+
+/**
+ * Change the blog name value.
+ *
+ * @param {string} value
+ */
+// wp.customize('blogname', value => {
+//   value.bind(to => $('.brand').text(to));
+// });
+
+/***/ }),
+
+/***/ 1:
+/*!******************************************************!*\
+  !*** multi ./resources/assets/scripts/customizer.js ***!
+  \******************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! /Users/edward/Desktop/wordpress/catalyst/wp-content/themes/catalyst/resources/assets/scripts/customizer.js */"./resources/assets/scripts/customizer.js");
+
+
+/***/ })
+
+},[[1,"/scripts/manifest"]]]);
+//# sourceMappingURL=customizer.js.map
